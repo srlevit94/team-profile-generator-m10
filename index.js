3462ebb205339c6ff3e25885d5f7ee840b854728
shanelevites <srlevit94@gmail.com>
2022-12-21 12:50:16 -0800

    fixed inquirer to add info; error with writehtml

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const generateHTML = require('./src/generateHTML.js');
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const Employee = require('./lib/employee');
 
 
 const teamRoster = [];
@@ -19,42 +20,45 @@ const addManager = () => {
         {
         type: "input",
         message: "Please enter your full name.",
-        name: 'name',
+        name: 'name'
         // validate not blank
         },
         {   
         message: "Please enter your employee ID.",
-        name: 'id',
+        name: 'id'
         //valudate not blank
         // validate numerical value
         },
         {   
         message: "Please enter your email address.",
-        name: 'email',
+        name: 'email'
         // validate not blank
         // check that email is valid
         },
         {   
         message: "Please enter your office number.",
-        name: 'officeNumber',
+        name: 'officeNumber'
         // validate not blank
         },
     ])
     // Saves input and adds manager variable to the team
-    .then(managerInfo => {
-        const {name, id, email, officeNumber} = managerInfo;
-        const manager = new Manager(name, id, email, officeNumber);
+    .then((managerInfo) => { 
+        const manager = new Manager (managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber);
         teamRoster.push(manager);
+        console.log(manager);
+        return addEmployee();
     })
 };
 
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 const addEmployee = () => {
+
     return inquirer.prompt([
         {
             type: "list",
             message: "Do you want to keep adding employess to this page?",
+            name: "role",
             choices:[
                 "Yes, add an Engineer.",
                 "Yes, add an Intern.",
@@ -86,8 +90,36 @@ const addEmployee = () => {
             message: "Please enter the Intern's school.",
             name: "email",
             when: (input) => input.role === "Intern"
+        },
+        {
+            type: 'confirm',
+            name: 'addMore',
+            message: 'Would you like to add more team members?',
+            default: false
         }
     ])
+
+    .then((employeeInfo) => {
+        let employee = "";
+
+        if (addEmployee.role === "Yes, add an Engineer.") {
+            employee = new Engineer (employeeInfo.name, employeeInfo.id, employeeInfo.email, employeeInfo.github);
+            console.log(employee);
+
+        } else if (addEmployee.role === "Yes, add an Intern.") {
+            employee = new Intern (employeeInfo.name, employeeInfo.id, employeeInfo.email, employeeInfo.school);
+            console.log(employee);
+        }
+
+        teamRoster.push(employee); 
+        console.log(employee);
+
+        if (addEmployee.addMore) {
+            return addEmployee(teamRoster); 
+        } else {
+            return writeHTML(teamRoster);
+        }
+    })
 }
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
@@ -100,13 +132,13 @@ const writeHTML = data => {
 // and I am taken back to the menu 
 // funtion to init app
 addManager()
-  .then(addEmployee)
-  .then(teamRoster => {
-    return writeHTML(teamRoster);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+//   .then(addEmployee)
+//   .then(teamRoster => {
+//     return writeHTML(teamRoster);
+//   })
+//   .then(pageHTML => {
+//     return writeFile(pageHTML);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
